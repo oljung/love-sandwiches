28eb0bcd761f378dc56c5fae7d5a82252bd1efb5
oljung <patrik_osterljung@hotmail.com>
2021-09-19 12:26:01 +0000

    feat: create validate data function

--- run.py
+++ run.py
@@ -21,7 +21,25 @@ def get_sales_data():
     print('Data should be in six figures seperated by a comma')
     print('Example: 10,20,30,40,50,60\n')
     data_str = input('Enter your data here: ')
-    print(f'The data input is {data_str}')
+
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, convert all values to integers.
+    Raise ValueError of the values can not be
+    converted or if there arn't six values
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f'Exactly 6 values required, you provided {len(values)}'
+                )
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again')
 
 
 get_sales_data()
