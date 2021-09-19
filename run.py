c1eaafb52e3488078f976d5914082fee296a833d
oljung <patrik_osterljung@hotmail.com>
2021-09-19 12:49:06 +0000

    feat: create update sales worksheet function to update spreadsheet with new data

--- run.py
+++ run.py
@@ -27,6 +27,7 @@ def get_sales_data():
         if validate_data(sales_data):
             print('Data input is valid')
             break
+    return sales_data
 
 
 def validate_data(values):
@@ -48,4 +49,17 @@ def validate_data(values):
     return True
 
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update the sales data in the spreadsheet
+    """
+    print('Updating sales worksheet\n')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('Sales worksheet updated successfully\n')
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+print(sales_data)
+update_sales_worksheet(sales_data)
